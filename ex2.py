5f0c0ab46e830d47dde537fa08d311baf0c8aa5f
Kat Reinhart <kat.reinhart@gmail.com>
2017-07-31 10:12:29 -0700

    Initial commit

---
+++ ex2.py
@@ -0,0 +1,5 @@
+# A comment, this is so you can read your program later.
+
+print "I could have code like this." # and the comment after is ignored.
+# print "This will not run."
+print "This will run."
---
+++ ex5.py
@@ -0,0 +1,15 @@
+name = 'Kat Reinhart'
+age = 34
+height = 68 #inches
+weight = 140 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Blonde'
+
+print "Let's talk about %s." % name
+print "She's %d inches tall." % height
+print "She weighs %d pounds." % weight
+print "She has %s eyes and %s hair." % (eyes, hair)
+print "Her teeth are usually %s depending on the coffee." % teeth
+
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
